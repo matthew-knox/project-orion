bc0d414f60baaffc251224ebb08e7698489b831b
Matthew Knox <mateuszknox@gmail.com>
2023-07-22 12:18:48 -0500

    relative paths

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new Discord.Client({
 
 // Event Handler
 console.log(chalk.bold.yellowBright('Loading Events'));
-const events = fs.readdirSync(`./events/`).filter(file => file.endsWith('.js'));
+const events = fs.readdirSync(`../project-orion/events/`).filter(file => file.endsWith('.js'));
 for (const file of events) {
     const event = require(`./events/${file}`);
     client.on(file.split('.')[0], event.bind(null, client));
@@ -25,7 +25,7 @@ for (const file of events) {
 // Message Command Handler
 console.log(chalk.bold.yellowBright('Loading Message Commands'));
 client.MessageCommands = new Discord.Collection();
-const messageCommands = fs.readdirSync(`./commands/messages/`).filter(files => files.endsWith('.js'));
+const messageCommands = fs.readdirSync(`../project-orion/commands/messages/`).filter(files => files.endsWith('.js'));
 for (const file of messageCommands) {
     const command = require(`./commands/messages/${file}`);
     client.MessageCommands.set(command.name.toLowerCase(), command);
@@ -35,7 +35,7 @@ for (const file of messageCommands) {
 // Slash Command Handler
 console.log(chalk.bold.yellowBright('Loading Slash Commands'));
 client.SlashCommands = new Discord.Collection();
-const slashCommands = fs.readdirSync(`./commands/interactions/`).filter(files => files.endsWith('.js'));
+const slashCommands = fs.readdirSync(`../project-orion/commands/interactions/`).filter(files => files.endsWith('.js'));
 for (const file of slashCommands) {
     const command = require(`./commands/interactions/${file}`);
     client.SlashCommands.set(command.data.name, command);
